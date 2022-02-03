670fbc8a9c5a1e60e8dbb3d0e3a5b1323dac5a3d
bodasooqa <timlol9894@gmail.com>
2022-02-03 18:44:05 +0300

    feature/iosint-4.9 [+] Add brute force method

--- Navigation/Login/LoginFactory/LoginInspector.swift
+++ Navigation/Login/LoginFactory/LoginInspector.swift
@@ -10,6 +10,7 @@ import CurrentUserService
 protocol LoginViewControllerDelegate {
     
     func checkCredentials(username: String, password: String) -> Bool
+    func bruteForce(passwordToUnlock: String)
     
 }
 
@@ -18,6 +19,40 @@ class LoginInspector: LoginViewControllerDelegate {
     public func checkCredentials(username: String, password: String) -> Bool {
         let checkerService = CheckerService.shared
         return checkerService.checkCredentials(username: username, password: password)
+    }
+    
+    public func bruteForce(passwordToUnlock: String) {
+        let ALLOWED_CHARACTERS: [String] = String().printable.map { String($0) }
+
+        var password: String = ""
+
+        while password != passwordToUnlock {
+            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
+        }
+    }
+    
+    private func indexOf(character: Character, _ array: [String]) -> Int {
+        array.firstIndex(of: String(character))!
+    }
+
+    private func characterAt(index: Int, _ array: [String]) -> Character {
+        index < array.count ? Character(array[index]) : Character("")
+    }
+
+    private func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
+        var str: String = string
+
+        if str.count <= 0 {
+            str.append(characterAt(index: 0, array))
+        } else {
+            str.replace(at: str.count - 1, with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
+
+            if indexOf(character: str.last!, array) == 0 {
+                str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
+            }
+        }
+
+        return str
     }
     
 }
