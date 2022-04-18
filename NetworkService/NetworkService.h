25221e7606e0d99bd68381f0689f56993b273b25
bodasooqa <timlol9894@gmail.com>
2022-04-19 01:26:02 +0300

    iosdt-1.1 [+] Add NetworkService

---
+++ NetworkService/NetworkService.h
@@ -0,0 +1,18 @@
+//
+//  NetworkService.h
+//  NetworkService
+//
+//  Created by t.lolaev on 18.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkService.
+FOUNDATION_EXPORT double NetworkServiceVersionNumber;
+
+//! Project version string for NetworkService.
+FOUNDATION_EXPORT const unsigned char NetworkServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkService/PublicHeader.h>
+
+
